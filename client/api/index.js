9e8c263625b99bd5cb5fb7368bb3af2e76311247
K.Hari Kishore Reddy <harikishorereddy9908@gmail.com>
2026-06-28 17:39:21 +0530

    Debug Vercel API crash

--- client/api/index.js
+++ client/api/index.js
@@ -1,2 +1,8 @@
-const app = require('../backend/app.js');
-module.exports = app;
+try {
+  const app = require('../backend/app.js');
+  module.exports = app;
+} catch (error) {
+  module.exports = (req, res) => {
+    res.status(500).json({ error: 'Serverless Function Boot Crash', details: error.message, stack: error.stack });
+  };
+}
